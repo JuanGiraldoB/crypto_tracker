272aa6b198fe2d46dd8d48edf655a3d065211fdb
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-08-18 20:41:52 -0500

    Added backtesting/pandas imports and backtesting functions

--- cryptotracker/tracker/views.py
+++ cryptotracker/tracker/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import JsonResponse
 from json import dumps
+from django.http import HttpResponse
+from django.urls import reverse
 
 # Others
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+from backtesting.test import SMA
+import pandas as pd
+
 
 # Models
 from tracker.models import Symbol, PriceData
@@ -38,9 +45,9 @@ class Chart(View):
     def chart_data(self, symbol="BTCUSDT"):
         symbol_id = Symbol.objects.filter(symbol=symbol).values("id")
         price_data = PriceData.objects.select_related('symbol').values_list(
-            'open', flat=True).filter(symbol_id=symbol_id[0]["id"])[::1]
+            'open', flat=True).filter(symbol_id=symbol_id[0]["id"])[:100:1]
         price_date = PriceData.objects.select_related('symbol').values_list(
-            'date', flat=True).filter(symbol_id=symbol_id[0]["id"])[::1]
+            'date', flat=True).filter(symbol_id=symbol_id[0]["id"])[:100:1]
 
         return symbol, price_data, price_date
 
@@ -54,7 +61,7 @@ class Chart(View):
                 'price': price_data,
                 'date': price_date
             })
-
+        print("da")
         symbol, price_data, price_date = self.chart_data()
         context = {'symbol': symbol,
                    'price': price_data,
@@ -63,3 +70,64 @@ class Chart(View):
         dataJSON = dumps(context, default=self.json_serial)
 
         return render(request, 'tracker/chart.html', {'data': dataJSON})
+
+
+class SmaCross(Strategy):
+    n1 = 50
+    n2 = 100
+
+    def init(self):
+        close = self.data.Close
+        self.sma1 = self.I(SMA, close, self.n1)
+        self.sma2 = self.I(SMA, close, self.n2)
+        print("***********0*************")
+
+    def next(self):
+        # print("***********1*************")
+        price = self.data.Close
+        # print("***********2*************")
+
+        if crossover(self.sma1, self.sma2):
+            sl = price - price * 0.03
+            tp = price + price * 0.04
+            self.buy(sl=sl, tp=tp)
+            # print("***********3*************")
+
+
+def resampleOHLC(df, interval):
+    df = df.resample(interval).agg({
+        'Open': 'first',
+        'High': 'max',
+        'Low': 'min',
+        'Close': 'last'
+    })
+    df = df.dropna()
+    return df
+
+
+def bts(request, interval):
+    returns = []
+
+    # Obtains symbols
+    symbols = Symbol.objects.values_list('id', flat=True)[::1]
+    # print(symbols)
+
+    time_frame = pd.to_datetime('today') - timedelta(days=40)
+    print(time_frame)
+    for symbol in symbols:
+        qry = PriceData.objects.select_related('symbol').filter(
+            date__lt=time_frame, symbol_id=symbol).values_list("date", "open", "high", "low", "close")
+        df = pd.DataFrame(
+            qry, columns=["Date", "Open", "High", "Low", "Close"]).set_index("Date")
+        df = resampleOHLC(df, "4H")
+        print(len(df.index))
+        bt = Backtest(df, SmaCross, cash=30000,
+                      commission=0.0015, exclusive_orders=True)
+        output = bt.run()
+        returns.append(output['Return [%]'])
+
+    frame = pd.DataFrame(returns, index=symbols, columns=['ret'])
+    print(frame)
+    return HttpResponse("hi")
+
+    # return redirect(reverse("tracker:chart"))
