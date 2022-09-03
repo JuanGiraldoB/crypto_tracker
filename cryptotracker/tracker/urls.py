282bf4997dc3aad33363f7c665f31b477651aa24
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-09-03 17:07:48 -0500

    Commented out list and detail view paths

--- cryptotracker/tracker/urls.py
+++ cryptotracker/tracker/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'tracker'
 urlpatterns = [
-    path('', views.SymbolListView.as_view(), name='all'),
-    path('symbol/<int:pk>/', views.SymbolDetailView.as_view(), name='symbol_detail'),
-    path('chart/', views.Chart.as_view())
+    #path('', views.SymbolListView.as_view(), name='all'),
+    #path('symbol/<int:pk>/', views.SymbolDetailView.as_view(), name='symbol_detail'),
+    path('', views.Chart.as_view(), name="all"),
 ]
